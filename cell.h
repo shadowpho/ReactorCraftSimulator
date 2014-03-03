f92558988f21eee158d3fcb008e3f1aa4ebf3d47
shadowpho <shadowp@gmail.com>
2014-03-03 01:25:33 -0800

    Initial checkin.

---
+++ cell.h
@@ -0,0 +1,21 @@
+
+/*
+ *Will be extended by
+ *
+ * 1) Fuel Rods
+ * 2) Steam boilers
+ * 3) Coolant Rod
+ * 4) Control Rod
+ * 5) Central Control Rod\Computer
+ * 6) Iron\Concrete
+ * 7) Air
+ * */
+
+
+
+
+struct cell
+{
+	signed int temperature; 
+
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "cell.h"
+
+
+const int LENGTH = 30;
+const int WIDTH = 30;
+
+const cell reactor_array[ LENGTH * WIDTH ];
+
+int main(int argc, char* argv[])
+{
+
+	return 1;
+}
+
+
+
